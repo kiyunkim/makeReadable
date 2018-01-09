b7582adaed1b534ec3c275e8de31ae5cd5d56315
K <kiyunkim@users.noreply.github.com>
2018-01-09 14:31:11 -0500

    notes in js, more inputs in html

--- script.js
+++ script.js
@@ -18,3 +18,20 @@ var makeReadablejs = (function(options) {
 
 var makeReadable = new makeReadablejs();
 makeReadable.setup();
+
+// store cookie to remember user's inputs
+
+/* for later - color picker
+input is fired on the <input> element every time the color changes. The change event is fired when the user dismisses the color picker. 
+check with "value"
+
+colorPicker.addEventListener("input", updateFirst, false);
+colorPicker.addEventListener("change", watchColorPicker, false);
+
+function watchColorPicker(event) {
+  document.querySelectorAll("p").forEach(function(p) {
+    p.style.color = event.target.value;
+  });
+}
+
+*/
