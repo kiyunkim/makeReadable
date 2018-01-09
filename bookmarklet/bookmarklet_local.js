f83153b07a1ab176e77db3bdff46af1d09a73d3e
K <kiyunkim@users.noreply.github.com>
2018-01-09 17:16:11 -0500

    fix bookmarklet.js + change local url to absolute

--- bookmarklet/bookmarklet_local.js
+++ bookmarklet/bookmarklet_local.js
@@ -1,7 +1,5 @@
 javascript:(function() { var mRjs = document.getElementsByClassName('makeReadable_js'); if(mRjs.length < 1) { var s = document.createElement("script"); s.src = "file:///Users/kimk/Documents/GitHub/kiyunkim.github.io/makeReadable/script.js"; s.setAttribute('class', 'makeReadable_js'); document.body.appendChild(s); } })()
 
-
-
 // formatted: 
 javascript: (function() {
   var mRjs = document.getElementsByClassName('makeReadable_js');
